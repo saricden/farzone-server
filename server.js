b709e1eb57792319543631f51eb688d2b0137a66
Kirk Morris <worker.kirk@gmail.com>
2022-05-08 03:10:46 -0700

    Switches for ExpressPeerServer.

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+const express = require('express');
+const { ExpressPeerServer } = require('peer');
+const app = express();
 const PORT = process.env.port || 9000;
 
-const { PeerServer } = require('peer');
+const server = app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+  console.log('Ctrl+C to quit, have fun!');
+});
 
-const peerServer = PeerServer({ port: PORT, path: '/farzone' });
+const peerServer = ExpressPeerServer(server, {
+  path: '/'
+});
+
+app.use('/', peerServer);
+
+module.exports = app;
