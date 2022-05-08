45b7840f788cc8685bcb703d9b5c9e432d8322f6
Kirk Morris <worker.kirk@gmail.com>
2022-05-08 03:15:18 -0700

    Oops. Capital PORT.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { ExpressPeerServer } = require('peer');
 const app = express();
-const PORT = process.env.port || 9000;
+const PORT = process.env.PORT || 9000;
 
 const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
